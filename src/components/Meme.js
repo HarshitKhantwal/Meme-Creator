7bf00c07feb52771ae633d38173a417815a0f317
HarshitKhantwal <106478085+HarshitKhantwal@users.noreply.github.com>
2022-11-15 19:08:24 +0530

    few changes in css

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import memeData from "../memeData";
 export default function Meme(){
+   const[meme,setMeme]=useState("")
+    function getImage(){
+        const memeArray=memeData.data.memes
+        const random=Math.floor(Math.random()*memeArray.length)
+        setMeme(memeArray[random].url)
+    }
     return(
-        <form>
+        <main>
+        <div className="form">
             <input className="form--input" placeholder="Top text" type="text"/>
             <input className="form--input" placeholder="Bottom text" type="text"/>
-            <button className="form--button">Get new meme image <img src="../../public/images/small.png" className="small--button"  alt=""/></button>
-        </form>
+            <button onClick={getImage} className="form--button">Get new meme image <img src="../../public/images/small.png" className="small--button"  alt=""/></button>
+        </div>
+        <img src={meme} alt="Meme" className="meme--image"/>
+        </main>
     )
 }
